7ae192711f19faa573bda4ee343c353261cbf77e
murphy-afk <bardarenoemi@gmail.com>
2026-04-01 11:24:52 +0200

    feat: card component

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,18 @@
 
 @section('content')
 
-<h1>test</h1>
+@php
+$comics = config('comics'); 
+@endphp
+
+<h1>Le Card:</h1>
+
+@foreach ($comics as $comic)
+<x-card>
+  <x-slot:title></x-slot:title>
+  <x-slot:description></x-slot:description>
+  <x-slot:price></x-slot:price>
+</x-card>
+@endforeach
 
 @endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,10 +4,13 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  @vite('resources/sass/app.scss', 'resources/js/app.js')
 </head>
 <body>
-  @include('partials.header')
-  @yield('content')
-  @include('partials.footer')
+  <div class="container">
+    @include('partials.header')
+    @yield('content')
+    @include('partials.footer')
+  </div>
 </body>
 </html>

11dd7472f3669339a78c95409e396df15851dcf0
murphy-afk <bardarenoemi@gmail.com>
2026-04-02 18:13:06 +0200

    feat: links section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,8 @@
   @php
     $comics = config('comics'); 
   @endphp
-  <div class="hero-section">
-
-  </div>
+  <div class="hero-section"></div>
+  <h2 class="badge">CURRENT SERIES</h2>
   <div class="container">
     <div class="row g-4 mt-4 ">
 
@@ -17,6 +16,25 @@
           <x-slot:title>{{$comic['series']}}</x-slot:title>
         </x-card>
       @endforeach
+    </div>
+    <button class="btn text-light d-block mx-auto mt-4 btn-load">LOAD MORE</button>
+  </div>
+  <div class="links-section mt-5 py-2 d-flex justify-content-between px-4">
+    <div class="d-flex align-items-center">
+      <img src={{ Vite::asset('resources/img/buy-comics-digital-comics.png') }} alt="logo">
+      <p class="text-light ms-2">DIGITAL COMICS</p>
+    </div>
+    <div class="d-flex align-items-center">
+      <img src={{ Vite::asset('resources/img/buy-comics-merchandise.png') }} alt="logo">
+      <p class="text-light ms-2">MERCHANDISE</p>
+    </div>
+    <div class="d-flex align-items-center">
+      <img src={{ Vite::asset('resources/img/buy-comics-shop-locator.png') }} alt="logo">
+      <p class="text-light ms-2">SHOP</p>
+    </div>
+    <div class="d-flex align-items-center">
+      <img src={{ Vite::asset('resources/img/buy-comics-subscriptions.png') }} alt="logo">
+      <p class="text-light ms-2">SUBSCRIPTIONS</p>
     </div>
   </div>
 

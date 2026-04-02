e37ac1ec5010adfb89a433d8e086ce32b42df367
murphy-afk <bardarenoemi@gmail.com>
2026-04-02 18:39:17 +0200

    feat: footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,48 @@
 <footer>
-  <p>
-    this is a footer
-  </p>
+  <div class="footer-top py-3 mt-0 text-light">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-md-3">
+          <p class="fw-bold">DC COMICS</p>
+          <ul class="list-unstyled">
+            <li>link</li>
+            <li>link</li>
+            <li>link</li>
+            <li>link</li>
+            <li>link</li>
+            <li>link</li>
+          </ul>
+          <p class="fw-bold mt-4">DC COMICS</p>
+          <ul class="list-unstyled">
+            <li>link</li>
+            <li>link</li>
+            <li>link</li>
+            <li>link</li>
+            <li>link</li>
+            <li>link</li>
+          </ul>
+        </div>
+        <div class="col-12 col-md-3">
+          <p class="fw-bold">DC COMICS</p>
+          <ul class="list-unstyled">
+            <li>link</li>
+            <li>link</li>
+            <li>link</li>
+            <li>link</li>
+            <li>link</li>
+            <li>link</li>
+          </ul>
+          <p class="fw-bold mt-4">DC COMICS</p>
+          <ul class="list-unstyled">
+            <li>link</li>
+            <li>link</li>
+            <li>link</li>
+            <li>link</li>
+            <li>link</li>
+            <li>link</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
 </footer>
